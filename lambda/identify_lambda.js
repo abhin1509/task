db487468cf3dc8d1d6df5aa010ff7794e17cc899
Abhinav Kumar <abhin1509@gmail.com>
2023-06-01 11:22:28 +0530

    handle email and phone number exist in two different contacts

--- lambda/identify_lambda.js
+++ lambda/identify_lambda.js
@@ -11,6 +11,7 @@ const sendResponse = (code, contacts) => {
   };
 };
 
+
 const checkContact = async (incomingEmail, phoneNumber, id) => {
   const res = await dynamoDB
     .scan({
@@ -20,7 +21,44 @@ const checkContact = async (incomingEmail, phoneNumber, id) => {
     .catch((error) => {
       console.error(error);
     });
-  
+
+  // check two different existing contacts
+  let setAns = new Set();
+  for (let curOrder of res.Items) {
+    if (curOrder.phoneNumber == phoneNumber) {
+      setAns.add(curOrder.id);
+    }
+    if (curOrder.email == incomingEmail) {
+      setAns.add(curOrder.id);
+    }
+  }
+  let firstId;
+  let secondId;
+  if (setAns.size == 2) {
+    // means email and phoneNumber from two different existing contacts
+    let obj = [...setAns];
+    firstId = obj[0];
+    secondId = obj[1];
+
+    let firstTime = new Date(firstId.createdAt);
+    let secondTime = new Date(secondId.createdAt);
+    let OlderId = (firstTime < secondTime) ? secondId : firstId;
+    // secondTime is older, so secondId is primary and firstId is secondary
+    // make firstId as secondary
+    await dynamoDB
+      .update({
+        TableName: TABLE_NAME,
+        Key: {
+          id: OlderId
+        },
+        UpdateExpression: `set linkPrecedence = :x`,
+        ExpressionAttributeValues: {
+          ":x": "secondary",
+        },
+      }).promise();
+  }
+
+
   let primaryContatctId = id;
   let primaryEmail = "";
   let primaryNumber = "";
@@ -31,41 +69,48 @@ const checkContact = async (incomingEmail, phoneNumber, id) => {
   let isCurrentContactPrimary = true;
 
   for (let currentOrder of res.Items) {
+    console.log("currentOrder:: ", currentOrder);
+    console.log(emailsList, phoneNumbersList);
     if (
       (currentOrder.email == incomingEmail && incomingEmail != null) ||
       (currentOrder.phoneNumber == phoneNumber && phoneNumber != null)
     ) {
+      console.log("inside if loop currentOrder:: ", currentOrder);
+      console.log(emailsList, phoneNumbersList);
       isCurrentContactPrimary = false;
       if (currentOrder.linkPrecedence == "primary") {
         primaryContatctId = currentOrder.id;
-        if(currentOrder.email) {  // check db email not null
+        if (currentOrder.email) { // check db email not null
           primaryEmail = currentOrder.email;
         }
-        if(currentOrder.phoneNumber) { // check if not null
+        if (currentOrder.phoneNumber) { // check if not null
           primaryNumber = currentOrder.phoneNumber; // check if phoneNumber exists
         }
-      } else {
-        if(currentOrder.email) {  // check db email not null
-          emailsList.add(currentOrder.email);
-        }
-        if(currentOrder.phoneNumber) { // check phone number not null
-          phoneNumbersList.add(currentOrder.phoneNumber);
-        }
+      }
+      else {
         secondaryContactIds.push(currentOrder.id);
+      }
+      if (currentOrder.email) { // check db email not null
+        emailsList.add(currentOrder.email);
+      }
+      if (currentOrder.phoneNumber) { // check phone number not null
+        phoneNumbersList.add(currentOrder.phoneNumber);
       }
     }
   }
+  console.log("emailsList:: ", emailsList);
+  console.log("phoneNumbersList:: ", phoneNumbersList);
 
   // adding current email and phone number to both list
   // bcoz in case of primary email or phone number is not present
-  if(incomingEmail) { //incoming email is not null
+  if (incomingEmail) { //incoming email is not null
     emailsList.add(incomingEmail);
   }
-  if(phoneNumber) { // incoming no is not null
+  if (phoneNumber) { // incoming no is not null
     phoneNumbersList.add(phoneNumber);
   }
-  
-  
+
+
   // if primaryContactId is not current id
   // means current id is also secondary id
   if (primaryContatctId != id) {
@@ -91,10 +136,10 @@ const checkContact = async (incomingEmail, phoneNumber, id) => {
 
   // if current contact is primary return same email and phone number
   if (isCurrentContactPrimary) {
-    if(phoneNumber) { // incoming no is not null
+    if (phoneNumber) { // incoming no is not null
       phoneNumbers.push(phoneNumber);
     }
-    if(incomingEmail) { //incoming email is not null
+    if (incomingEmail) { //incoming email is not null
       emails.push(incomingEmail);
     }
   }
@@ -133,18 +178,21 @@ exports.handler = async (event) => {
       deletedAt,
     };
     console.log("record:: ", record);
-    await dynamoDB
-      .put({
-        Item: record,
-        TableName: TABLE_NAME,
-      })
-      .promise()
-      .catch((error) => {
-        console.error(error);
-      });
+    if (phoneNumber || email) {
+      await dynamoDB
+        .put({
+          Item: record,
+          TableName: TABLE_NAME,
+        })
+        .promise()
+        .catch((error) => {
+          console.error(error);
+        });
+    }
     console.log("response: ", responseItem);
     return sendResponse(200, responseItem);
-  } catch (e) {
+  }
+  catch (e) {
     console.log(e);
   }
 };
